bba9adc17d42a378960b92cef8d4ba9e031044f6
Ivan Lopes <ivan.lopes@live.fr>
2020-11-05 13:30:12 +0100

    Separate resizing and scaling in BasicDataset

--- datasets/BasicDataset.py
+++ datasets/BasicDataset.py
@@ -6,58 +6,65 @@ from PIL import Image
 from utils import list_images
 
 class BasicDataset(torch.utils.data.Dataset):
-  """
-  Single image dataset fetcher
-  """
-  def __init__(self, images_directory, scale_factor=2, memory_fit_factor=4, training=True, resize=None):
-    self.image_paths = list_images(images_directory)
-    self.training = training
-    self.scale_factor = scale_factor
-    self.memory_factor = memory_fit_factor
-    self.resize = resize
+    """
+    Single image dataset fetcher
+    """
+    def __init__(self, images_directory, scale_factor=2, memory_fit_factor=4, training=True, resize=None):
+        self.image_paths = list_images(images_directory)
+        self.training = training
+        self.scale_factor = scale_factor
+        self.memory_factor = memory_fit_factor
+        self.resize = resize
 
-  def __getitem__(self, index):
-    original = Image.open(self.image_paths[index]).convert('RGB')
-    width, height = original.width, original.height
+    def __getitem__(self, index):
+        """
+            index -> image -> resize -> scale -> ?augment -> (input, label)
+        """
+        original = Image.open(self.image_paths[index]).convert('RGB')
+        width, height = original.width, original.height
 
-    if self.training:
-      resize_height = height // self.memory_factor
-      resize_width = width // self.memory_factor
-      while resize_height * resize_width > 393 * 510:  # avoid too big tensors
-        resize_height -= self.memory_factor
-        resize_width -= int(self.memory_factor * (width / height))
-    else:
-      resize_height = height
-      resize_width = width
+        if self.resize is not None: # force resize with arbitrary values
+            resize_height, resize_width = self.resize
 
-    resize_height -= resize_height % self.scale_factor
-    resize_width -= resize_width % self.scale_factor
+        elif self.training:
+            resize_height = height // self.memory_factor
+            resize_width = width // self.memory_factor
 
-    resize_dimensions = (resize_height, resize_width)
+            while resize_height * resize_width > 393 * 510:  # avoid too big tensors for GPU
+                resize_height -= self.memory_factor
+                resize_width -= int(self.memory_factor * (width / height))
 
-    augmentation = T.Compose([
-      T.RandomPerspective(),
-      T.RandomRotation((-15, 15)),
-      T.RandomResizedCrop(resize_dimensions, scale=(0.5, 0.9), interpolation=Image.BICUBIC),
-      T.RandomHorizontalFlip(0.3),
-      T.RandomVerticalFlip(0.3)
-    ])
+        else:
+            resize_height = height
+            resize_width = width
 
-    resize_factors = (resize_height // self.scale_factor, resize_width // self.scale_factor)
+        resize_height -= resize_height % self.scale_factor
+        resize_width -= resize_width % self.scale_factor
+        resized = (resize_height, resize_width)
 
-    if self.resize is not None: # force resize
-      resize_factors = self.resize
+        augmentation = T.Compose([
+            T.RandomPerspective(),
+            T.RandomRotation((-15, 15)),
+            T.RandomResizedCrop(resized, scale=(0.5, 0.9), interpolation=Image.BICUBIC),
+            T.RandomHorizontalFlip(0.3),
+            T.RandomVerticalFlip(0.3)
+        ])
 
-    # print('original size (%i,%i)' % (width, height))
-    # print('size (%i,%i)' % (resize_factors[0],resize_factors[1]))
-    resize = [T.Resize(resize_factors, interpolation=Image.BICUBIC)]
-    to_tensor = [T.ToTensor()]
+        scaled = (resized[0] // self.scale_factor, resized[1] // self.scale_factor)
 
-    image = augmentation(original) if self.training else original
-    return T.Compose(resize + to_tensor)(image), T.Compose(to_tensor)(image)
+        # print('original:', (height, width))
+        # print('resized:', resized)
+        # print('scaled:', scaled)
 
-  def __len__(self):
-    return len(self.image_paths)
+        resize = [T.Resize(resized, interpolation=Image.BICUBIC)]
+        resize_and_scale = [T.Resize(scaled, interpolation=Image.BICUBIC)]
+        to_tensor = [T.ToTensor()]
 
-  def get_image_name(self, index):
-    return self.image_paths[index]
+        image = augmentation(original) if self.training else original
+        return T.Compose(resize + to_tensor)(image), T.Compose(resize_and_scale + to_tensor)(image)
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def get_image_name(self, index):
+        return self.image_paths[index]
