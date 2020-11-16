5312586a954de939db310618a7771f326643d755
Ivan Lopes <ivan.lopes@live.fr>
2020-11-16 09:48:36 +0100

    Add few-shot training sets

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -4,6 +4,12 @@ datasets = {
     'DIV2K_valid': '../data/DIV2K/DIV2K_valid',
     'CelebA_train': '../data/CelebA/CelebA_train',
     'CelebA_valid': '../data/CelebA/CelebA_valid',
+    'CelebA_fs1': '../data/CelebA/CelebA_fs1',
+    'CelebA_fs2': '../data/CelebA/CelebA_fs2',
+    'CelebA_fs3': '../data/CelebA/CelebA_fs3',
     'CFW_train': '../data/CFW/CFW_train',
-    'CFW_valid': '../data/CFW/CFW_valid'
+    'CFW_valid': '../data/CFW/CFW_valid',
+    'CFW_fs1': '../data/CFW/CFW_fs1',
+    'CFW_fs2': '../data/CFW/CFW_fs2',
+    'CFW_fs3': '../data/CFW/CFW_fs3',
 }
