c8047a8253326211535a47bf071baeb6dfd18333
Ivan Lopes <ivan.lopes@live.fr>
2020-12-24 10:46:04 +0100

    Make sweep runs same length regardless of duration

--- sweep.py
+++ sweep.py
@@ -11,11 +11,14 @@ parser.add_argument('sweep_id', type=str, help='Wandb sweep id.')
 sweep_id = parser.parse_args().sweep_id
 
 def train():
-    run = wandb.init()
-    config = dict(run.config)
+    trial = wandb.init()
+    
+    config = dict(trial.config)
     config['update_test_steps'] = config['update_steps']
     config['size'] = [config['size']]*2
+    config['epochs'] = int(config['epochs']*config['nb_tasks'])    
+
     opt = Namespace(**config)
-    MetaTrain(opt)()
+    MetaTrain(opt)(name=trial.name)
 
 wandb.agent(sweep_id, train, project='FSSR')
