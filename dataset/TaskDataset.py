5919bcc0c9660fc11c7a890bfc2f2a0d2071ed42
Ivan Lopes <ivan.lopes@live.fr>
2020-12-27 17:19:17 +0100

    Small refactor

--- dataset/TaskDataset.py
+++ dataset/TaskDataset.py
@@ -9,23 +9,23 @@ from utils import list_images, fetch_image
 
 class TaskDataset(DatasetWrapper):
     """Style-based task segmentation of the dataset."""
-    style_params = {'b': 0.5, 'c': 0.5, 's': 0.5, 'h': 0.5}
+    style_params = {'b': 0.4, 'c': 0.4, 's': 0.4, 'h': 0.2}
 
     def __init__(self, fp, scale, size, shots, augment=False, style=False, shuffle=False):
         self.fp = fp
         self.sizes = t.get_sizes(size, scale)
         self.style = style
-        self.shots = shots
         self.augment_name = augment
         self.augment = t.augment(augment, self.sizes[0])
         self.scale = t.resize(self.sizes[1])
 
         if shuffle:
             random.shuffle(self.paths)
+        paths = list_images(datasets[fp] if fp in datasets else fp) # remove self.
         task_size = shots + 1
-        paths = list_images(datasets[fp] if fp in datasets else fp)
-        self.tasks = [paths[i: i + task_size] for i in range(0, len(paths), task_size)]
-
+        n_tasks = len(paths) // task_size
+        self.tasks = [paths[i: i + task_size] for i in range(0, n_tasks*task_size, task_size)]
+        
     def __getitem__(self, index):
         imgs = [fetch_image(fp) for fp in self.tasks[index]]
 
