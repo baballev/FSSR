5f2b4bd722fd0882a3dde62322ebaf933c405857
Ivan Lopes <ivan.lopes@live.fr>
2020-12-30 12:48:31 +0100

    New sweep incoming

--- sweep.py
+++ sweep.py
@@ -8,17 +8,20 @@ from FSSR import MetaTrain
 
 parser = ArgumentParser()
 parser.add_argument('sweep_id', type=str, help='Wandb sweep id.') 
-sweep_id = parser.parse_args().sweep_id
+args = parser.parse_args()
 
 def train():
     trial = wandb.init()
     
     config = dict(trial.config)
-    config['update_test_steps'] = config['update_steps']
     config['size'] = [config['size']]*2
-    config['epochs'] = int(config['epochs']*config['nb_tasks'])    
-
+    
     opt = Namespace(**config)
     MetaTrain(opt)(name=trial.name)
 
-wandb.agent(sweep_id, train, project='FSSR')
+#opt={'clusters': 'DIV2K_160clusters', 'debug': False, 'epochs': 0, 'first_order': True, 'load': False, 'loss': 'L1', 'lr': 0.000792658750105796, 'lr_annealing': 0, 'meta_lr': 0.00019211474409957373, 'n_feats': 64, 'n_resblocks': 16, 'nb_tasks': 1, 'scale': 2, 'shots': 1, 'size': [248, 248], 'timesteps': 10000, 'train_set': 'DIV2K_all#AUGMENTOR', 'update_steps': 10, 'update_test_steps': 10, 'wandb': False, 'weight_decay': 1e-06}
+#opt['debug'] = True
+
+#opt = Namespace(**opt)
+#MetaTrain(opt)()
+wandb.agent(args.sweep_id, train, project='FSSR')
