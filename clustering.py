fcaddb387a7a2c5c1fee5ccf5ab4b1b95d09728f
Ivan Lopes <ivan.lopes@live.fr>
2021-01-13 23:30:29 +0100

    PCA fit and embedding+transform

--- clustering.py
+++ clustering.py
@@ -12,10 +12,12 @@ from PIL import Image
 
 parser = ArgumentParser()
 
-parser.add_argument('mode', choices=['tile', 'embed', 'pca', 'cluster'])
+parser.add_argument('mode', choices=['tile', 'embed', 'pca-fit', 'pca-transform', 'cluster'])
 parser.add_argument('--path', type=str, default='../data/DIV2K/DIV2K_valid')
+parser.add_argument('--pca', type=str, help='File containing the pickled PCA.')
 parser.add_argument('--out', type=str)
-parser.add_argument('--sample', type=float, default=0.1, help='Propotion of data points used.') 
+parser.add_argument('--sample', type=float, default=0.1, help='Propotion of data points used.')
+parser.add_argument('--n-components', type=int, default=100, help='Number of components in the PCA.')
 parser.add_argument('--n-clusters', type=int, default=80)
 parser.add_argument('--min-size', type=int, default=10, help='Minimum cluster size.')
 parser.add_argument('--max-size', type=int, default=20, help='Maximum cluster size.')
@@ -56,38 +58,76 @@ elif opt.mode == 'embed': # use @torch.no_grad()
     model = torchvision.models.vgg16(pretrained=True)
     model.classifier = nn.Sequential(*[model.classifier[i] for i in range(4)])
     model.eval().to(device)
-    
+
     filenames = os.listdir(opt.path)
     samples = random.sample(filenames, int(len(filenames)*opt.sample))
     embs = {}
 
-    import pdb; pdb.set_trace()
-
     with torch.no_grad():
-        for f in tqdm(filenames):
+        for f in tqdm(samples):
             name, _ = os.path.splitext(f)
             img = Image.open(os.path.join(opt.path, f))
             x = T.ToTensor()(img).unsqueeze(0).to(device)
-        
-            embs[f] = model(x).detach().cpu().numpy()
 
-    #dest = '%s_%i_emb' % (opt.path, opt.patch_size)
-    dest = './emb'
-    np.save(dest, embs)
-    print(f'embedded {len(samples)} of the {len(filenames)} found into {dest}.npy')
+            emb = model(x).detach().cpu().numpy()
+            embs[f] = emb
+
+    np.save(f'{opt.path}_{emb.shape[1]}emb_{len(samples)}', embs)
+    print(f'embedded {len(samples)} of the {len(filenames)} images found')
 
 
-elif opt.mode == 'pca':
-    from sklearn.decomposition import PCA    
+elif opt.mode == 'pca-fit':
+    # will fit on the entire content of opt.fit
+    from sklearn.decomposition import PCA
+    import json
+
     assert os.path.isfile(opt.path)
 
+    name, _ = os.path.splitext(opt.path)
+
     embs = np.load(opt.path, allow_pickle=True).item()
-    img_names = embs.keys()
+    X = np.concatenate([emb for emb in embs.values()])
 
-    fit = random.sample(img_names, len(img_names)*opt.fit)
-    X = np.stack([embs[name] for name in fit])
+    pca = PCA(n_components=opt.n_components)
+    pca.fit(X)
 
-    import pdb; pdb.set_trace()
+    np.save(f'{name}_{opt.n_components}pca', pca)
+
+    cumsum_variance = np.cumsum(pca.explained_variance_ratio_)
+    json.dump(str(list(cumsum_variance)), 
+        open(f'{name}_{opt.n_components}pca-var.txt', 'w'))
+    print(f'PCA fit with {opt.n_components} components on {len(X)} vectors ended')
+
+
+elif opt.mode == 'pca-transform':
+    import torch
+    import torch.nn as nn
+    import torchvision
+    import torchvision.transforms as T
+    assert os.path.isdir(opt.path) and os.path.isfile(opt.pca)
+
+    name, _ = os.path.splitext(opt.path)
+    pca = np.load(opt.pca, allow_pickle=True).item()
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = torchvision.models.vgg16(pretrained=True)
+    model.classifier = nn.Sequential(*[model.classifier[i] for i in range(4)])
+    model.eval().to(device)
+
+    filenames = os.listdir(opt.path)
+    projections = {}
+    with torch.no_grad():
+        for f in tqdm(filenames):
+            #name, _ = os.path.splitext(f)
+            img = Image.open(os.path.join(opt.path, f))
+            x = T.ToTensor()(img).unsqueeze(0).to(device)
+
+            emb = model(x).detach().cpu().numpy()
+            projected = pca.transform(emb)
+            projections[f] = projected
+
+    np.save(f'{name}_{pca.n_components}pca', projections)
+    print(f'embedded and projected w/ PCA {len(filenames)} images')
 
 
 elif opt.mode == 'cluster':
