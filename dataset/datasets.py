e61fcf23fd3c1ddff5bcdfe76cc46747d238ae4c
Ivan Lopes <ivan.lopes@live.fr>
2021-01-17 22:14:37 +0100

    Add 400-clustered DIV2K 256x256 tiles

--- dataset/datasets.py
+++ dataset/datasets.py
@@ -1,10 +1,12 @@
 """Dataset directories relative to project root directory."""
 datasets = {
     'DIV2K_all': '../data/DIV2K/DIV2K_all',
+    'DIV2K_256tiles': '../data/DIV2K/DIV2K_256tiles',
     'DIV2K_train': '../data/DIV2K/DIV2K_train',
     'DIV2K_valid': '../data/DIV2K/DIV2K_valid',
     'DIV2K_60clusters': '../data/DIV2K/DIV2K_train_cluster60.npy',
     'DIV2K_160clusters': '../data/DIV2K/DIV2K_train_cluster160.npy',
+    'DIV2K_400clusters': '../data/DIV2K/DIV2K_256tiles_300pca_400clusters.npy',
     'CelebA_train': '../data/CelebA/CelebA_train',
     'CelebA_valid': '../data/CelebA/CelebA_valid',
     'CelebA_fs1': '../data/CelebA/CelebA_fs1',
