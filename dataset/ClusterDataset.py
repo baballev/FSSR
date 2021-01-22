c1d379cc2a29454417cac4b7511091a0a8f88099
Ivan Lopes <ivan.lopes@live.fr>
2021-01-22 14:56:27 +0100

    Improvements on metea-datasets

--- dataset/ClusterDataset.py
+++ dataset/ClusterDataset.py
@@ -2,7 +2,7 @@ import random
 import os
 
 import torch
-import numpy as np
+from torchvision.datasets.folder import default_loader
 
 import dataset.transform as t
 from .datasets import datasets
@@ -13,16 +13,30 @@ class ClusterDataset(DatasetWrapper):
     """Style-based task segmentation of the dataset."""
     style_params = {'b': 0.3, 'c': 0.5, 's': 0.3, 'h': 0.4}
 
-    def __init__(self, fp, clusters, scale, size, shots, augment=False, style=False, random=True):
-        self.clusters = [c for c in clusters if len(c) >= shots + 1]
+    def __init__(self, fp, clusters, scale, size, spt_size, qry_size, augment=False, style=False, 
+        random=True, loader=default_loader, strict=False):
+        
         self.fp = datasets[fp] if fp in datasets else fp
-        self.shots = shots
+        self.loader = lambda x: loader(os.path.join(self.fp, x))
+
+        self.spt_size, self.qry_size = spt_size, qry_size
+        
+        self.clusters = self.check_clusters(clusters, strict)
         self.style = style
         self.random = random
         self.augment_name = augment
         self.sizes = t.get_sizes(size, scale)    
         self.augment = t.augment(augment, self.sizes[0])
         self.scale = t.resize(self.sizes[1])
+
+    def check_clusters(self, clusters, strict):
+        sizes = [len(x) >= self.shots + 1 for x in clusters]
+    
+        if strict:
+            assert all(sizes)
+
+        return [x for x, big_enough in zip(clusters, sizes) if big_enough]
+        
 
     def __getitem__(self, index):
         cluster = list(self.clusters[index])
@@ -32,7 +46,7 @@ class ClusterDataset(DatasetWrapper):
             support = cluster[:self.shots]
         
         samples = support + cluster[-1:]
-        imgs = [fetch_image(os.path.join(self.fp, path)) for path in samples]
+        imgs = [self.loader(fp) for fp in samples]
 
         p = t.Pipeline(self.augment)
         if self.style:
