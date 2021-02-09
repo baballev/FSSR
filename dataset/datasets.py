ed2bf7cb9abf69a4a7e07415e37a138f960c803d
Ivan Lopes <ivan.lopes@live.fr>
2021-02-09 14:04:01 +0100

    Dataset cleanup

--- dataset/datasets.py
+++ dataset/datasets.py
@@ -9,12 +9,6 @@ datasets = {
     'DIV2K_400clusters': '../data/DIV2K/DIV2K_256tiles_300pca_400clusters.npy',
     'CelebA_train': '../data/CelebA/CelebA_train',
     'CelebA_valid': '../data/CelebA/CelebA_valid',
-    'CelebA_fs1': '../data/CelebA/CelebA_fs1',
-    'CelebA_fs2': '../data/CelebA/CelebA_fs2',
-    'CelebA_fs3': '../data/CelebA/CelebA_fs3',
     'CFW_train': '../data/CFW/CFW_train',
     'CFW_valid': '../data/CFW/CFW_valid',
-    'CFW_fs1': '../data/CFW/CFW_fs1',
-    'CFW_fs2': '../data/CFW/CFW_fs2',
-    'CFW_fs3': '../data/CFW/CFW_fs3',
 }
